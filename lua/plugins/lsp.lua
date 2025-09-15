a47dfe0ba17d86de0b8f47f4c88a3c26ad93933b
Tavis Palmer <tavis@tavispalmer.com>
2025-09-14 23:29:13 -0400

    Simplify

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,38 +6,18 @@ return {
       "mason.nvim",
       { "mason-org/mason-lspconfig.nvim", config = function() end },
     },
-    opts = function()
-      local ret = {
-        diagnostics = {
-          virtual_text = {
-            prefix = "●",
-          },
-        },
-        inlay_hints = {
-          enabled = true,
-        },
-      }
-      return ret
-    end,
+    opts = {
+      diagnostics = {
+        virtual_text = {},
+      },
+    },
     config = function(_, opts)
-      -- diagnostics signs
-      if vim.fn.has("nvim-0.10.0") == 0 then
-        if type(opts.diagnostics.signs) ~= "boolean" then
-          for severity, icon in pairs(opts.diagnostics.signs.text) do
-            local name = vim.diagnostic.severity[severity]:lower():gsub("^%l", string.upper)
-            name = "DiagnosticSign" .. name
-            vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
-          end
-        end
-      end
-
       vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
     end,
   },
 
   -- cmdline tools and lsp servers
   {
-
     "mason-org/mason.nvim",
     opts = {}
   },
