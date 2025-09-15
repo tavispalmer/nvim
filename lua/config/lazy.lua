600d14e73322e93c080eb64e422bce9418d6ab0e
Tavis Palmer <tavis@tavispalmer.com>
2025-09-14 23:03:14 -0400

    Add lsp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,3 +14,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+})
+
+require("config.options")
---
+++ lua/config/options.lua
@@ -0,0 +1,6 @@
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+
+-- lsp
+vim.lsp.enable('clangd')
